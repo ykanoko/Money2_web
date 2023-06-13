d9be86646f01183a6269fab612a03ba0a33afa58
ykanoko <kano7101d.pg@gmail.com>
2023-06-13 22:20:48 +0900

    ブラウザで"Hello, world!"を表示

---
+++ backend/go.mod
@@ -0,0 +1,25 @@
+module github.com/ykanoko/Money2_web/backend
+
+go 1.20
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo-jwt v0.0.0-20221127215225-c84d41a71003
+)
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/labstack/echo/v4 v4.9.2-0.20221125112752-466bf80e418b // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/time v0.2.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
+	// "context"
+	// "fmt"
+	// "io"
+	// "net/http"
+	// "os"
+	// "os/signal"
+	// "time"
+	// "github.com/golang-jwt/jwt"
+	// "github.com/labstack/echo"
+	// echojwt "github.com/labstack/echo-jwt"
+	// "github.com/labstack/echo/middleware"
+	// "github.com/ykanoko/Money2_web/backend/db"
+	// "github.com/ykanoko/Money2_web/backend/handler"
+)
+
+/*
+複数行の
+コメント
+*/
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func root(c echo.Context) error {
+	res := Response{Message: "Hello, world!"}
+	return c.JSON(http.StatusOK, res)
+}
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Logger.SetLevel(log.INFO)
+
+	front_url := os.Getenv("FRONT_URL")
+	if front_url == "" {
+		front_url = "http://localhost:3000"
+	}
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{front_url},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+
+	// Routes
+	e.GET("/", root)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":9000"))
+}
+
+const (
+	exitOK = iota
+	exitError
+)
+
+// func main() {
+// 	os.Exit(run(context.Background()))
+// }
+
+// func run(ctx context.Context) int {
+// 	e := echo.New()
+
+// 	// Middleware
+// 	e.Use(middleware.Recover())
+
+// 	logfile := os.Getenv("LOGFILE")
+// 	if logfile == "" {
+// 		logfile = "access.log"
+// 	}
+// 	lf, _ := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+// 	logger := middleware.LoggerWithConfig(middleware.LoggerConfig{
+// 		Format: logFormat(),
+// 		Output: io.MultiWriter(os.Stdout, lf),
+// 	})
+// 	e.Use(logger)
+
+// 	frontURL := os.Getenv("FRONT_URL")
+// 	if frontURL == "" {
+// 		frontURL = "http://localhost:3000"
+// 	}
+// 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+// 		AllowOrigins: []string{frontURL},
+// 		AllowMethods: []string{"GET", "PUT", "DELETE", "OPTIONS", "POST"},
+// 	}))
+// 	e.Use(middleware.BodyLimit("5M"))
+
+// 	// jwt
+// 	config := echojwt.Config{
+// 		NewClaimsFunc: func(c echo.Context) jwt.Claims {
+// 			return new(handler.JwtCustomClaims)
+// 		},
+// 		SigningKey: []byte(handler.GetSecret()),
+// 	}
+
+// 	// db
+// 	sqlDB, err := db.PrepareDB(ctx)
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "failed to prepare DB: %s\n", err)
+// 		return exitError
+// 	}
+// 	defer sqlDB.Close()
+
+// 	h := handler.Handler{
+// 		DB:        sqlDB,
+// 		UserRepo:  db.NewUserRepository(sqlDB),
+// 		MoneyRepo: db.NewItemRepository(sqlDB),
+// 	}
+
+// 	// Routes
+// 	e.POST("/initialize", h.Initialize)
+// 	e.GET("/log", h.AccessLog)
+
+// 	// e.GET("/items", h.GetOnSaleItems)
+// 	// e.GET("/items/:itemID", h.GetItem)
+// 	// e.GET("/items/:itemID/image", h.GetImage)
+// 	// e.GET("/items/categories", h.GetCategories)
+// 	// e.POST("/register", h.Register)
+// 	// e.POST("/login", h.Login)
+
+// 	// Login required
+// 	l := e.Group("")
+// 	l.Use(echojwt.WithConfig(config))
+
+// 	// l.GET("/users/:userID/items", h.GetUserItems)
+// 	// l.POST("/items", h.AddItem)
+// 	// l.POST("/sell", h.Sell)
+// 	// l.POST("/purchase/:itemID", h.Purchase)
+// 	// l.GET("/balance", h.GetBalance)
+// 	// l.POST("/balance", h.AddBalance)
+
+// 	// Start server
+// 	go func() {
+// 		if err := e.Start(":9000"); err != nil && err != http.ErrServerClosed {
+// 			e.Logger.Fatal("shutting down the server")
+// 		}
+// 	}()
+
+// 	quit := make(chan os.Signal, 1)
+// 	signal.Notify(quit, os.Interrupt)
+// 	<-quit
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
+// 	if err := e.Shutdown(ctx); err != nil {
+// 		e.Logger.Fatal(err)
+// 	}
+
+// 	return exitOK
+// }
+
+func logFormat() string {
+	// Customize freely: https://echo.labstack.com/guide/customization/
+	var format string
+	format += "time:${time_rfc3339}\t"
+	format += "status:${status}\t"
+	format += "method:${method}\t"
+	format += "uri:${uri}\t"
+	format += "latency:${latency_human}\t"
+	format += "error:${error}\t"
+	format += "\n"
+
+	// Other log choice
+	// - json format
+	// `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}",` +
+	// 	`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+	// 	`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"` +
+	// 	`,"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n"
+	// - structured logging:  https://pkg.go.dev/golang.org/x/exp/slog
+
+	return format
+}
