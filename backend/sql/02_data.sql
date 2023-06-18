9db266e9450ffd52e5eaa10629ad006977b440e3
ykanoko <kano7101d.pg@gmail.com>
2023-06-18 16:24:24 +0900

    POST initialize 完成

--- backend/sql/02_data.sql
+++ backend/sql/02_data.sql
@@ -1,7 +1,6 @@
 BEGIN TRANSACTION;
 
 INSERT INTO "money2" (id, type_id, user_id, amount, calculation_user1) VALUES(1, 0, 0, 0, 0);
-"INSERT INTO users (name, password) VALUES (?, ?)", user.Name, user.Password
 -- TODO:sign up時に初期金額を登録するようにすれば、↑はいらないかも
 
 INSERT INTO "types" VALUES(1,'収入');
